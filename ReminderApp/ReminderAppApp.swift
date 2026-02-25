b75b9b2ffdb601f79321205a76b1d86e3b4b7e79
hamno996 <hamnomhsen@gmail.com>
2026-02-25 15:51:50 +0300

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Hamno Muhsin on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
